d31244fc011cf0d83c3b9be478751936cd18d5af
Nicholas Ferrara <nferrara100@gmail.com>
2022-02-08 20:57:13 +0000

    refactor: renamed variables in word stats

--- word-statistics/calculate.py
+++ word-statistics/calculate.py
@@ -2,8 +2,8 @@ import os
 import re
 
 
-def stats_for_file(file, word):
-    pattern = f"\s{re.escape(word)}[^a-z]"
+def stats_for_file(file, query):
+    pattern = f"\s{re.escape(query)}[^a-z]"
     script_dir = os.path.dirname(__file__)
     abs_file_path = os.path.join(script_dir, file)
     with open(abs_file_path) as f:
@@ -12,21 +12,21 @@ def stats_for_file(file, word):
         matches = len(results)
         examples = []
         if matches > 0:
-            sentence_pattern = f"[^.]*\s{re.escape(word)}[^a-z.]*[^.]*"
+            sentence_pattern = f"[^.]*\s{re.escape(query)}[^a-z.]*[^.]*"
             sentences = re.findall(sentence_pattern, lines, flags=re.IGNORECASE)
             for sentence in sentences:
                 examples.append(" ".join(sentence.split()) + ".")
         return {"matches": matches, "examples": examples}
 
 
-def get_word_stats(word):
+def get_stats(query):
     count = 0
     examples = []
     in_documents = []
     for i in range(1, 7):
         document_name = f"doc{i}.txt"
         document_path = f"data/{document_name}"
-        stats = stats_for_file(document_path, word)
+        stats = stats_for_file(document_path, query)
         if stats["matches"] > 0:
             count += stats["matches"]
             examples += stats["examples"]
--- word-statistics/test_calculate.py
+++ word-statistics/test_calculate.py
@@ -1,24 +1,24 @@
-from calculate import get_word_stats
+from calculate import get_stats
 
 
 def test_correct_count():
-    stats = get_word_stats("me")
+    stats = get_stats("me")
     assert stats["count"] == 35
     assert len(stats["examples"]) == 35
 
 
 def test_multiple_word_query():
-    stats = get_word_stats("Old State Capitol")
+    stats = get_stats("Old State Capitol")
     assert stats["in_documents"] == ["doc1.txt"]
 
 
 def test_simple_example_sentence():
-    stats = get_word_stats("Thank")
+    stats = get_stats("Thank")
     assert "Thank you." in stats["examples"]
 
 
 def test_long_example_sentence():
-    stats = get_word_stats("biological")
+    stats = get_stats("biological")
     assert (
         "And we need to work together to obtain a bilateral agreement on biological threat reduction."
         in stats["examples"]
@@ -26,7 +26,7 @@ def test_long_example_sentence():
 
 
 def test_word_end_of_sentence():
-    stats = get_word_stats("kiev")
+    stats = get_stats("kiev")
     assert (
         "We were in Ukraine, visiting a pathogen laboratory in Kiev."
         in stats["examples"]
@@ -34,12 +34,12 @@ def test_word_end_of_sentence():
 
 
 def test_case_insensitive():
-    stats = get_word_stats("DiFfIcUlT")
+    stats = get_stats("DiFfIcUlT")
     assert "It will be difficult." in stats["examples"]
 
 
 def test_nonalphabetic_char():
-    stats = get_word_stats("my family's")
+    stats = get_stats("my family's")
     assert (
         "In many ways, then, my family's life reflects some of the contradictions of Kenya, and indeed, the African continent as a whole."
         in stats["examples"]
