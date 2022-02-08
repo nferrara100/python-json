291b9d8d815b7d1650b1772181de11bd047fc399
Nicholas Ferrara <nferrara100@gmail.com>
2022-02-08 20:00:46 +0000

    feat: working basic word searcher

--- word-statistics/calculate.py
+++ word-statistics/calculate.py
@@ -2,18 +2,34 @@ import os
 import re
 
 
-def number_of_in(word, file):
+def stats_for_file(file, word):
     pattern = f"\s{re.escape(word)}[^a-z]"
     script_dir = os.path.dirname(__file__)
     abs_file_path = os.path.join(script_dir, file)
     with open(abs_file_path) as f:
         lines = f.read()
         results = re.findall(pattern, lines, flags=re.IGNORECASE)
-        return len(results)
+        matches = len(results)
+        examples = []
+        if matches > 0:
+            sentence_pattern = f"[^.]*\s{re.escape(word)}[^a-z][^.]*"
+            sentences = re.findall(sentence_pattern, lines, flags=re.IGNORECASE)
+            for sentence in sentences:
+                examples.append(" ".join(sentence.split()) + ".")
+        return {"matches": matches, "examples": examples}
 
 
-def number_of(word):
+def get_word_stats(word):
     count = 0
-    for i in range(1, 6):
-        count += number_of_in(word, f"data/doc{i}.txt")
-    return count
+    examples = []
+    in_documents = []
+    for i in range(1, 7):
+        document_name = f"doc{i}.txt"
+        document_path = f"data/{document_name}"
+        stats = stats_for_file(document_path, word)
+        if stats["matches"] > 0:
+            count += stats["matches"]
+            examples += stats["examples"]
+            in_documents.append(document_name)
+
+    return {"count": count, "in_documents": in_documents, "examples": examples}
--- word-statistics/test_calculate.py
+++ word-statistics/test_calculate.py
@@ -1,5 +1,20 @@
-from calculate import number_of
+from calculate import get_word_stats
 
 
-def test_num_word_me():
-    assert number_of("me") == 33
+def test_count_word_me():
+    assert get_word_stats("me")["count"] == 35
+
+
+def test_state_capital_in_docs():
+    assert get_word_stats("Old State Capitol")["in_documents"] == ["doc1.txt"]
+
+
+def test_example_word_azerbaijan():
+    assert "Thank you." in get_word_stats("Thank")["examples"]
+
+
+def test_example_word_biological():
+    assert (
+        "And we need to work together to obtain a bilateral agreement on biological threat reduction."
+        in get_word_stats("biological")["examples"]
+    )
