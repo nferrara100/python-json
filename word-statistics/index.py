25d22035026357c62dc1229a9256d8d298159791
Nicholas Ferrara <nferrara100@gmail.com>
2022-02-09 17:58:35 +0000

    refactor: printer now own file

--- word-statistics/index.py
+++ word-statistics/index.py
@@ -6,53 +6,7 @@
 
 import argparse
 
-from calculate import get_query_stats
-from tabulate import tabulate
-
-
-# Formats stats as required for display.
-def get_formatted_stats(stats, query):
-    # Never show more than three examples if there are more.
-    num_examples = len(stats["examples"])
-    shown_examples = 3
-    if num_examples < 3:
-        shown_examples = num_examples
-    examples = "\n\n".join(stats["examples"][:shown_examples])
-    # return as list instead of dict.
-    return [f"{query}({stats['count']})", ", ".join(stats["locations"]), examples]
-
-
-# Returns two dimensional lists representing the grid of our desired output.
-def get_stat_grid(queries, source):
-    # Start with headers.
-    results = [
-        [
-            "Word or Phrase\n(Total Occurrences):",
-            "Documents:",
-            "Sentences containing the word:",
-        ],
-        ["-", "-", "-"],
-    ]
-
-    # Do this for every word of phrase searched and create its own row.
-    for query in queries:
-        # Do the calculations
-        stats = get_query_stats(query, source)
-        formatted_stats = get_formatted_stats(stats, query)
-        results.append(formatted_stats)
-        results.append(["-", "-", "-"])
-    return results
-
-
-# Gets output from helper and prints to standard out.
-def print_stats(queries, source):
-    stat_grid = get_stat_grid(queries, source)
-    output = tabulate(
-        stat_grid,
-        tablefmt="plain",
-    )
-    print(output)
-
+from printer import print_stats
 
 # Run this if not being imported. Accepts input from the command line and calls helper.
 if __name__ == "__main__":
