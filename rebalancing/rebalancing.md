af20d7c60252f59102ff9a212fa84e66d2fb70eb
Nicholas Ferrara <nferrara100@gmail.com>
2023-07-24 14:35:58 +0100

    style: format

--- rebalancing/rebalancing.md
+++ rebalancing/rebalancing.md
@@ -1,18 +1,21 @@
-Account Balancing
-=========================
- - The primary objective of the task is to write a working and correct solution.
- - Any optimisations are nice to have and if you feel that doing them upfront is detrimental to the primary objective they can be added later.
+# Account Balancing
+
+-   The primary objective of the task is to write a working and correct solution.
+-   Any optimisations are nice to have and if you feel that doing them upfront is
+    detrimental to the primary objective they can be added later.
 
 We have a number of different bank accounts that we use to fund transfers.
 
-From time-to-time, we need to "rebalance" these accounts.
-Rebalancing involves transferring funds from one or more accounts to another account.
+From time-to-time, we need to "rebalance" these accounts. Rebalancing involves
+transferring funds from one or more accounts to another account.
 
-Please write a function that takes an input (current_state, target_state) and returns [money_movements].
+Please write a function that takes an input (current_state, target_state) and returns
+[money_movements].
 
 The algorithm must:
-- Not send money to and from the same account.
-- Not output more than 1 money movement from and to the same pair of accounts.
+
+-   Not send money to and from the same account.
+-   Not output more than 1 money movement from and to the same pair of accounts.
 
 ```json
 current_state = [
--- rebalancing/rebalancing.py
+++ rebalancing/rebalancing.py
@@ -1,40 +1,17 @@
 current_state = [
-    {
-        "account_id": 1,
-        "balance": 1_000_000
-    },
-    {
-        "account_id": 2,
-        "balance": 2000_000
-    },
-    {
-        "account_id": 3,
-        "balance": 1000_000
-    },
-    {
-        "account_id": 4,
-        "balance": 2000_000
-    },
+    {"account_id": 1, "balance": 1_000_000},
+    {"account_id": 2, "balance": 2000_000},
+    {"account_id": 3, "balance": 1000_000},
+    {"account_id": 4, "balance": 2000_000},
 ]
 
 target_state = [
-    {
-        "account_id": 1,
-        "balance": 800_000
-    },
-    {
-        "account_id": 2,
-        "balance": 2500_000
-    },
-    {
-        "account_id": 3,
-        "balance": 1200_000
-    },
-    {
-        "account_id": 4,
-        "balance": 1500_000
-    },
+    {"account_id": 1, "balance": 800_000},
+    {"account_id": 2, "balance": 2500_000},
+    {"account_id": 3, "balance": 1200_000},
+    {"account_id": 4, "balance": 1500_000},
 ]
+
 
 def moveMoney(current, target):
     changes = []
@@ -46,13 +23,16 @@ def moveMoney(current, target):
             for x in range(0, len(current)):
                 if changes[x] > 0 and x != i:
                     apply = min(changes[x], abs(changes[i]))
-                    money_movements.append({"from_account": i + 1, "to_account": x + 1, "amount": apply})
+                    money_movements.append(
+                        {"from_account": i + 1, "to_account": x + 1, "amount": apply}
+                    )
                     changes[i] += apply
                     changes[x] -= apply
                     if changes[i] == 0:
                         break
 
     return money_movements
+
 
 final = moveMoney(current_state, target_state)
 
